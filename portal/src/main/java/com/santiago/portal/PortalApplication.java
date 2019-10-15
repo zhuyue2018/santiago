582c20163b60fbabbdabf1444744a7df93fa4082
zhuyue <372826057@qq.com>
2019-10-15 17:29:48 +0800

    1015

--- portal/src/main/java/com/santiago/portal/PortalApplication.java
+++ portal/src/main/java/com/santiago/portal/PortalApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
 
-@SpringBootApplication(scanBasePackages = {"com.santiago.portal", "com.santiago.core", "com.santiago.notify"})
-@MapperScan({"com.santiago.core.mapper", "com.santiago.portal.mapper", "com.santiago.notify.mapper"})
+@SpringBootApplication(scanBasePackages = {"com.santiago.portal"})
+@MapperScan({"com.santiago.portal.mapper"})
 public class PortalApplication {
     public static void main(String[] args) {
         SpringApplication.run(PortalApplication.class, args);
