26a77382fbde6af4f69a2b7ca9dc062cca9aa07a
zhuyue <372826057@qq.com>
2019-10-18 17:02:20 +0800

    1015

--- portal/src/main/java/com/santiago/portal/aop/WebLogAcpect.java
+++ portal/src/main/java/com/santiago/portal/aop/WebLogAcpect.java
@@ -21,36 +21,9 @@ public class WebLogAcpect {
 
     private Logger logger = LoggerFactory.getLogger(WebLogAcpect.class);
 
-//    /**
-//     * 定义切入点，切入点为com.santiago.portal.controller下的所有函数
-//     */
 //    @Pointcut("execution(public * com.santiago.portal.controller.*.*(..))")
-//    public void webLog() {
-//    }
-//
-//    /**
-//     * 前置通知：在连接点之前执行的通知
-//     *
-//     * @param joinPoint
-//     * @throws Throwable
-//     */
 //    @Before("webLog()")
-//    public void doBefore(JoinPoint joinPoint) throws Throwable {
-//        // 接收到请求，记录请求内容
-//        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-//        HttpServletRequest request = attributes.getRequest();
-//        logger.info("URL : " + request.getRequestURL().toString());
-//        logger.info("HTTP_METHOD : " + request.getMethod());
-//        logger.info("IP : " + request.getRemoteAddr());
-//        logger.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
-//        logger.info("ARGS : " + Arrays.toString(joinPoint.getArgs()));
-//    }
-
 //    @AfterReturning(returning = "ret", pointcut = "webLog()")
-//    public void doAfterReturning(Object ret) throws Throwable {
-//        // 处理完请求，返回内容
-//        logger.info("RESPONSE : " + ret);
-//    }
 
 
     @Before("@annotation(com.santiago.portal.annotation.LogParams)")
@@ -66,7 +39,7 @@ public class WebLogAcpect {
     }
 
     @AfterReturning(returning = "ret", pointcut = "@annotation(com.santiago.portal.annotation.LogParams)")
-    public void doAfterReturning2(Object ret) throws Throwable {
+    public void doAfterReturning(Object ret) throws Throwable {
         // 处理完请求，返回内容
         logger.info("RESPONSE : " + ret);
     }
