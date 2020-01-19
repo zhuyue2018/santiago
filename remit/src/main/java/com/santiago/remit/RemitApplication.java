6a1878218aff2574930d21e36de8ee41c45b9f5c
zhuyue <372826057@qq.com>
2020-01-19 16:05:41 +0800

    1103

---
+++ remit/src/main/java/com/santiago/remit/RemitApplication.java
@@ -0,0 +1,13 @@
+package com.santiago.remit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemitApplication.class, args);
+    }
+
+}
---
+++ remit/src/test/java/com/santiago/remit/RemitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santiago.remit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
